eb1839b9c63945a7cd7d38f14ba61f8c2311d5cb
Sunny <sunnysinghal2303@gmail.com>
2016-10-30 21:52:58 +0530

    Created the project

---
+++ online_food_ordering/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'online_food_ordering',  # Or path to database file if using sqlite3.
+        'USER': 'sunny',  # Not used with sqlite3.
+        'PASSWORD': 'joshlabs',  # Not used with sqlite3.
+        'HOST': 'localhost',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
+    }
+}
