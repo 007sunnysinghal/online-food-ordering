921a67db182baebeefab971cc03279bca9ceaad6
Sunny <sunnysinghal2303@gmail.com>
2016-10-31 15:02:47 +0530

    modified requirements file

--- online_food_ordering/local_settings.py
+++ online_food_ordering/local_settings.py
@@ -1,3 +1,5 @@
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -9,3 +11,15 @@ DATABASES = {
         'PORT': '5432',  # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
